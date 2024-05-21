7e2c8bcb50a88ef438fcb0b31ad79689272b6be8
Daniel Dolberg <66477511+DanielDolberg@users.noreply.github.com>
2024-05-21 16:13:52 +0300

    changed hello.java

--- DanielDolberg.java
+++ DanielDolberg.java
@@ -1,7 +1,7 @@
 public class DanielDolberg{
     public static void main(String[] args) {
         for (int i=1;i<=10; i++) {
-            System.out.println("Daniel Dolberg");
+            System.out.println("Daniel Dolberg is not a real person");
         }
     }
 }
